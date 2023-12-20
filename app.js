af67c7facdca019f91d3f7d55b1dcde7d21a0f94
Filip Popendyk <filipos28511@gmail.com>
2023-12-20 06:08:20 -0500

    plans the endpoints

--- app.js
+++ app.js
@@ -7,6 +7,28 @@ app.get('/', (req, res) => {
     
 })
 
+/*
+
+    ENDPOINTS 
+
+    /register - used for registering an user
+
+    /login - used for login
+
+    /products - used for retrieving all products data
+
+    /products/id - used for retrieving specific product data
+
+    /users - used for retrieving all users data
+
+    /users/id - used for retrieving specific user data
+
+    /carts - used for retrieving all carts data
+
+    /carts/id - used for retrieving specific cart data
+
+*/
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 })
